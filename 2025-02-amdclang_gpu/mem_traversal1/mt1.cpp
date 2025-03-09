d1b01e8df003c9dacff6ff332356339e23051a69
Igor Sfiligoi <isfiligoi@sdsc.edu>
2025-03-09 11:55:57 -0700

    Minor logic change

--- 2025-02-amdclang_gpu/mem_traversal1/mt1.cpp
+++ 2025-02-amdclang_gpu/mem_traversal1/mt1.cpp
@@ -15,20 +15,20 @@ int main(int argc, const char *argv[]) {
 
 	auto t0 = std::chrono::high_resolution_clock::now();
 	// indexes to the next element, fixed 4G buffer
-	uint32_t *idxs = new uint32_t[1000000000];
+	uint32_t *idxs = new uint32_t[0x40000000];
 #pragma omp parallel for
-	for (uint32_t i5=0; i5<5; i5++) { // do several parallel streams, as it is not trivial
-	  std::mt19937 myRandomGenerator(std::stol(argv[2])+i5);
-          for (uint32_t i=0; i<200000000; i++) idxs[i+i5*200000000] = myRandomGenerator() % 1000000000;
+	for (uint32_t i8=0; i8<8; i8++) { // do several parallel streams, as it is not trivial
+	  std::mt19937 myRandomGenerator(std::stol(argv[2])+i8);
+          for (uint32_t i=0; i<0x8000000; i++) idxs[(i8*0x8000000)+i] = myRandomGenerator() & 0x3fffffff;
 	}
-#pragma omp target enter data map(to:idxs[0:1000000000])
+#pragma omp target enter data map(to:idxs[0:0x40000000])
 
 	constexpr uint32_t n_comp = 4*1024-128; // 128-aligned, but not a multiple of 1k
 	uint32_t *buf = new uint32_t[n_els];
 
 	auto t1 = std::chrono::high_resolution_clock::now();
 #ifdef OMPGPU
-#pragma omp target teams distribute parallel for simd map(from:buf[0:n_els]) map(to:idxs[0:1000000000])
+#pragma omp target teams distribute parallel for simd map(from:buf[0:n_els]) map(to:idxs[0:0x40000000])
 #else
 #pragma omp parallel for simd
 #endif
@@ -45,14 +45,14 @@ int main(int argc, const char *argv[]) {
 	// sort is here just to make all the buffer relevant
 	std::sort(buf,buf+n_els);
 	auto t3 = std::chrono::high_resolution_clock::now();
-        printf("Result: %i\n",int(buf[0]));
+        printf("Results: %i %i %i\n",int(buf[0]),int(buf[n_els/2]),int(buf[n_els-1]));
         auto time_span0 = std::chrono::duration_cast<std::chrono::duration<double>>(t1 - t0);
         auto time_span1 = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
         auto time_span2 = std::chrono::duration_cast<std::chrono::duration<double>>(t3 - t2);
 	printf("compute %.3f s init %.3f sort %.3f s\n", time_span1.count(), time_span0.count(), time_span2.count());
 
 	delete[] buf;
-#pragma omp target exit data map(release:idxs[0:1000000000])
+#pragma omp target exit data map(release:idxs[0:0x40000000])
 	delete[] idxs;
 	return 0;
 }
